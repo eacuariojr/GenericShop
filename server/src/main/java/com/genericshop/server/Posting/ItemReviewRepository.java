f2fcabe3e57de037d66a7e8c9b8eebb5ae858493
ErwinA <acuario.erwin00@gmail.com>
2021-04-16 18:25:24 -0700

    Removed duplicated directories

--- server/src/main/java/com/genericshop/server/Posting/ItemReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.genericshop.server.posting;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemReviewRepository extends JpaRepository<ItemReview, Integer>
-{
-}
--- server/src/main/java/com/genericshop/server/User/VendorReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.genericshop.server.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VendorReviewRepository extends JpaRepository<VendorReview, Integer>
-{
-}
