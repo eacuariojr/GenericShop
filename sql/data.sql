a25109b0aa5c5e412d78a836f720a9b8c318d663
ErwinA <acuario.erwin00@gmail.com>
2021-04-20 16:05:30 -0700

    Created SQL file to populate schema for testing

---
+++ sql/data.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO user VALUES (1, 'nook.tom@genericshop.com', 'Tom', '2021-03-30 17:00:00.000', 'Nook', 'images/blank_profile.png', '2021-01-01 09:00:00.000');
+INSERT IGNORE INTO user VALUES (2, 'buyer@genericshop.com', 'Bob', '2021-04-19 13:05:23.000', 'Buyer', 'images/blank_profile.png', '2021-01-02 04:02:34.000');
+INSERT IGNORE INTO user VALUES (3, 'visitor@genericshop.com', 'Victor', '2021-01-03 12:52:32.000', 'Visitor', 'images/blank_profile.png', '2021-01-03 12:56:51.543');
+
+INSERT IGNORE INTO item_listing VALUES (1, 'A lovely mahogany table', 'images/pic01.jpg', '2021-01-03 12:56:51.543', 'Mahogany Table', '2021-01-01 10:00:00.000', "250.00", '5', 1);
+
+INSERT IGNORE INTO item_review VALUES (1, '2021-02-14 15:23:40.000', 5, 'I got this chair to impress my valentine and she loved it!', 1, 2);
